972ae37d409615e1d9dffa2620ab648a37832b99
tejashreethosar <tejashreethosar14@gmail.com>
2021-11-24 00:48:10 +0530

    .Net HelloWorld

---
+++ 03-DotNet/SomeClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class SomeClass
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World...!!!");
+		Console.WriteLine("Goodbye World...!!!");
+	}
+}
+
+/*
+
+STEPS TO RUN:
+1. csc.exe SomeClass.cs
+2. Execute SomeClass.exe
+
+*/
